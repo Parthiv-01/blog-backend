2e20afdf0a43e1366ac29ef989911cc594f69dab
parthiv-01 <parthivsadhukhan@gmail.com>
2025-04-05 23:34:54 +0530

    Reset git cache and updated .gitignore

---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from rest_framework import generics, permissions
+from .models import BlogPost
+from .serializers import BlogPostSerializer
+
+class BlogPostListCreateView(generics.ListCreateAPIView):
+    queryset = BlogPost.objects.all().order_by('-created_at')
+    serializer_class = BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class BlogPostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.request.method in ['PUT', 'PATCH', 'DELETE']:
+            return [permissions.IsAuthenticated()]
+        return super().get_permissions()
