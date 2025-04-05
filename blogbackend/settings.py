56ee6cbce4c78384231a1a9236fa452664fd64a5
parthiv-01 <parthivsadhukhan@gmail.com>
2025-04-06 01:41:41 +0530

    Reset git cache and updated .gitignore and worked

--- blogbackend/settings.py
+++ blogbackend/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 load_dotenv()
 
@@ -63,19 +64,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blogbackend.wsgi.application'
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
-    }
+    'default': dj_database_url.parse(
+        'postgresql://blogdb_1zek_user:NPqQ3G1E6vCkxjhS3bUATcxpPhYRWpmE@dpg-cvojm43uibrs73bsho0g-a.oregon-postgres.render.com/blogdb_1zek',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 REST_FRAMEWORK = {
